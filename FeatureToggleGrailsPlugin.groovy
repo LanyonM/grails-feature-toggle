c28123c2bd834cbbeef3e3bdacf105ed2d347b7c
Mike Lanyon <mlanyon@criticalmass.com>
2013-08-12 22:58:58 -0500

    clean up syntax

--- FeatureToggleGrailsPlugin.groovy
+++ FeatureToggleGrailsPlugin.groovy
@@ -1,59 +1,56 @@
-import grails.plugin.featuretoggle.FeatureToggleService;
+import grails.plugin.featuretoggle.FeatureToggleService
 
 class FeatureToggleGrailsPlugin {
-    // the plugin version
-    def version = "0.5-SNAPSHOT"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.7 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
+	// the plugin version
+	def version = "0.5-SNAPSHOT"
+	// the version or versions of Grails the plugin is designed for
+	def grailsVersion = "1.3.7 > *"
+	// the other plugins this plugin depends on
+	def dependsOn = [:]
+	// resources that are excluded from plugin packaging
+	def pluginExcludes = [
+			"grails-app/views/error.gsp"
+	]
 
-    // TODO Fill in these fields
-    def author = "Your name"
-    def authorEmail = ""
-    def title = "Plugin summary/headline"
-    def description = '''\\
+	// TODO Fill in these fields
+	def author = "Your name"
+	def authorEmail = ""
+	def title = "Plugin summary/headline"
+	def description = '''\\
 Brief description of the plugin.
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/grails-feature-toggle"
+	// URL to the plugin's documentation
+	def documentation = "http://grails.org/plugin/grails-feature-toggle"
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
+	def doWithWebDescriptor = { xml ->
+		// TODO Implement additions to web.xml (optional), this event occurs before
+	}
 
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
+	def doWithSpring = {
+		// TODO Implement runtime spring config (optional)
+	}
 
-    def doWithDynamicMethods = { ctx ->
-		
-  		FeatureToggleService service = ctx.getBean('featureToggleService')
-  		
-  		for(controllerClass in application.controllerClasses) {
-        service.enhance(controllerClass)
-      }
-  		
-      for(serviceClass in application.serviceClasses) {
-        service.enhance(serviceClass)
-      }
-    }
+	def doWithDynamicMethods = { ctx ->
+		FeatureToggleService service = ctx.getBean('featureToggleService')
+		for(controllerClass in application.controllerClasses) {
+			service.enhance(controllerClass)
+		}
+		for(serviceClass in application.serviceClasses) {
+			service.enhance(serviceClass)
+		}
+	}
 
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
+	def doWithApplicationContext = { applicationContext ->
+		// TODO Implement post initialization spring config (optional)
+	}
 
-    def onChange = { event ->
-      doWithDynamicMethods event.ctx
-    }
+	def onChange = { event ->
+		doWithDynamicMethods event.ctx
+	}
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+	def onConfigChange = { event ->
+		// TODO Implement code that is executed when the project configuration changes.
+		// The event is the same as for 'onChange'.
+	}
 }
--- grails-app/conf/grails/feature/toggle/filters/FeatureToggleFilters.groovy
+++ grails-app/conf/grails/feature/toggle/filters/FeatureToggleFilters.groovy
@@ -1,7 +1,7 @@
 package grails.feature.toggle.filters
-import grails.plugin.featuretoggle.FeatureToggleService;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import grails.plugin.featuretoggle.FeatureToggleService
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
 
 import grails.plugin.feature.toggle.annotations.Feature
 
@@ -15,36 +15,34 @@ class FeatureToggleFilters implements ApplicationContextAware {
 	def filters = {
 		allControllers(controller:'*', action:'*') {
 			before = {
-        def artefact = grailsApplication.getArtefactByLogicalPropertyName("Controller", controllerName)
-        def controller = applicationContext.getBean(artefact.clazz.name)
-        def annotation = controller.class.getAnnotation(Feature)
-        
-        if(annotation != null && !featureToggleService.isFeatureEnabled(annotation.name())) {
-          if(annotation.responseRedirect().size() > 0) {
-            redirect(uri: annotation.responseRedirect())
-            return false;
-          } else {
-            render(status: annotation.responseStatus())
-            return false;
-          }
-        }
+				def artefact = grailsApplication.getArtefactByLogicalPropertyName('Controller', controllerName)
+				def controller = applicationContext.getBean(artefact.clazz.name)
+				def annotation = controller.class.getAnnotation(Feature)
 
-        Method[] methods = controller.class.getMethods()
-        methods.each{ method ->
+				if (annotation != null && !featureToggleService.isFeatureEnabled(annotation.name())) {
+					if (annotation.responseRedirect().size() > 0) {
+						redirect(uri: annotation.responseRedirect())
+						return false
+					} else {
+						render(status: annotation.responseStatus())
+						return false
+					}
+				}
 
-          if(method.name == actionName) {
-            def methodAnnotation = method.getAnnotation Feature
-            if(methodAnnotation != null && !featureToggleService.isFeatureEnabled(methodAnnotation.name())) {
-              if(methodAnnotation.responseRedirect().size() > 0) {
-                redirect(uri: methodAnnotation.responseRedirect())
-                return false;
-              } else {
-                render(status: methodAnnotation.responseStatus())
-                return false;
-              }
-            }
-          }
-        }
+				Method[] methods = controller.class.getMethods().each { method ->
+					if (method.name == actionName) {
+						def methodAnnotation = method.getAnnotation(Feature)
+						if (methodAnnotation != null && !featureToggleService.isFeatureEnabled(methodAnnotation.name())) {
+							if (methodAnnotation.responseRedirect().size() > 0) {
+								redirect(uri: methodAnnotation.responseRedirect())
+								return false
+							} else {
+								render(status: methodAnnotation.responseStatus())
+								return false
+							}
+						}
+					}
+				}
 			}
 		}
 	}
