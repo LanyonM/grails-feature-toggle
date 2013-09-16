883c26cbf575d8488e70a656e84241e2cecec313
Conor Sullivan <conors@criticalmass.com>
2013-09-16 16:12:49 -0500

    Updating the tests (but they still don't pass)

--- grails-app/controllers/grails/feature/toggle/SampleController.groovy
+++ grails-app/controllers/grails/feature/toggle/SampleController.groovy
@@ -10,19 +10,47 @@ class SampleController {
 		return [result: 'success!']
 	}
 
-	def toggleInside() {
-		def ret = 'didn\'t run'
+  def toggleInside() {
+    def ret = 'didn\'t run'
 
-		withFeature('code') { ->
-			log.debug('the feature must be enabled')
-			ret = 'ran with feature'
-		}
+    withFeature('code') { ->
+      log.debug('the feature must be enabled')
+      ret = 'ran with feature'
+    }
 
-		withoutFeature('code') { ->
-			log.debug('the feature must not be enabled')
-			ret = 'ran without feature'
-		}
+    withoutFeature('code') { ->
+      log.debug('the feature must not be enabled')
+      ret = 'ran without feature'
+    }
 
-		return [result: ret]
-	}
+    return [result: ret]
+  }
+
+  def useWithElseConstruct() {
+    def ret = 'didn\'t run'
+
+    withFeature('code', { ->
+      log.debug('the feature must be enabled')
+      ret = 'ran with feature'
+    }, { ->
+      log.debug('the feature must not be enabled')
+      ret = 'ran without feature'
+    })
+
+    return [result: ret]
+  }
+
+  def useWithoutElseConstruct() {
+    def ret = 'didn\'t run'
+
+    withoutFeature('code', { ->
+      log.debug('the feature must be enabled')
+      ret = 'ran without feature'
+    }, { ->
+      log.debug('the feature must not be enabled')
+      ret = 'ran with feature'
+    })
+
+    return [result: ret]
+  }
 }
--- test/unit/grails/feature/toggle/SampleControllerTests.groovy
+++ test/unit/grails/feature/toggle/SampleControllerTests.groovy
@@ -14,12 +14,13 @@ import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
 import grails.test.mixin.web.FiltersUnitTestMixin
 
+import groovy.mock.interceptor.StubFor
 
 /**
  * This test is actually used to test the FeatureToggleFilters implementation by
  * testing the SampleController with that filter.
  *
- * @author mminella
+ * @author mlanyon, conors
  */
 @TestFor(SampleController)
 @Mock(FeatureToggleFilters)
@@ -30,22 +31,14 @@ class SampleControllerTests {
 
 	@Before
 	void setup() {
-		// def featureService = mockFor(FeatureToggleService)
-		// featureService.demand.isFeatureEnabled { String feature -> return true }
-		// featureToggleService = featureService.createMock()
 		defineBeans{
-			// featureToggleServiceControl(GrailsMock, FeatureToggleService)
-			// featureToggleService(featureToggleServiceControl: 'createMock')
 			featureToggleService(FeatureToggleService)
 		}
-		// def featureToggleServiceControl = applicationContext.getBean('featureToggleServiceControl')
-		// featureToggleServiceControl.demand.isFeatureEnabled = { String feature -> return true }
 		featureToggleService = applicationContext.getBean('featureToggleService')
 		featureToggleService.grailsApplication = grailsApplication
 
-		// featureToggleService.enhance(controller.class)
-		def controllerMock = mockFor(SampleController)
-		controllerMock.demand.static.withFeature { String featureName, Closure closure ->
+		def controller = new SampleController()
+		/*controllerMock.demand.static.withFeature { String featureName, Closure closure ->
 			if(featureToggleService.isFeatureEnabled(featureName)) {
 				closure.call()
 			}
@@ -57,12 +50,12 @@ class SampleControllerTests {
 		}
 		controllerMock.demand.static.featureEnabled { String featureName ->
 			return featureToggleService.isFeatureEnabled(featureName)
-		}
-		controller = controllerMock.createMock()
+		}*/
+    featureToggleService.enhance(controller)
 
 		assertNotNull 'controller should not be null', controller
 		assertNotNull 'featureToggleService should not be null', featureToggleService
-		// assertTrue 'controller should have withFeature method', controller.class.getMethods().findAll { it.name.contains('withFeature') }.size() > 0
+		//assertTrue 'controller should have withFeature method', controller.class.getMethods().findAll { it.name.contains('withFeature') }.size() > 0
 	}
 
 	@Test
@@ -75,27 +68,32 @@ class SampleControllerTests {
 		println controller.modelAndView.model.result
 	}
 
-	@Test
-	void testActionIsDisabled() {
-		// def featureService = mockFor(FeatureToggleService)
-		// featureService.demand.isFeatureEnabled {String feature -> return false}
-		boolean featureState = featureToggleService.isFeatureEnabled('action')
-		// this should work, but doesn't
-		featureToggleService.setFeatureEnabled('action', false)
-		// so we've done this instead
-		featureToggleService.grailsApplication.config.features.action.enabled = false
-		println "action feature is ${featureToggleService.isFeatureEnabled('action')}"
-		withFilters(action: 'index') {
-			controller.index()
-		}
-		// return the value to what it used to be
-		// this should work, but doesn't
-		featureToggleService.setFeatureEnabled('action', featureState)
-		// so we've done this instead
-		featureToggleService.grailsApplication.config.features.action.enabled = featureState
-		println "action feature is ${featureToggleService.isFeatureEnabled('action')}"
-		assert response.status == 404
-	}
+  @Test
+  void testControllerIsDisabled() {
+    boolean featureState = featureToggleService.isFeatureEnabled('action')
+    featureToggleService.setFeatureEnabled('controller', false)
+    println "action feature is ${featureToggleService.isFeatureEnabled('action')}"
+    withFilters(action: 'index') {
+      controller.index()
+    }
+    // return the value to what it used to be
+    featureToggleService.setFeatureEnabled('controller', featureState)
+    println "action feature is ${featureToggleService.isFeatureEnabled('action')}"
+    assert response.status == 404
+  }
+  @Test
+  void testActionIsDisabled() {
+    boolean featureState = featureToggleService.isFeatureEnabled('action')
+    featureToggleService.setFeatureEnabled('action', false)
+    println "action feature is ${featureToggleService.isFeatureEnabled('action')}"
+    withFilters(action: 'index') {
+      controller.index()
+    }
+    // return the value to what it used to be
+    featureToggleService.setFeatureEnabled('action', featureState)
+    println "action feature is ${featureToggleService.isFeatureEnabled('action')}"
+    assert response.status == 404
+  }
 
 	@Test
 	void testCodeIsEnabled() {
@@ -107,24 +105,70 @@ class SampleControllerTests {
 		println controller.modelAndView.model.result
 	}
 
-	@Test
-	void testCodeisDisabled() {
-		boolean featureState = featureToggleService.isFeatureEnabled('code')
-		// this should work, but doesn't
-		featureToggleService.setFeatureEnabled('code', false)
-		// so we've done this instead
-		featureToggleService.grailsApplication.config.features.code.enabled = false
-		withFilters(action: 'toggleInside') {
-			controller.toggleInside()
-		}
-		// return the value to what it used to be
-		// this should work, but doesn't
-		featureToggleService.setFeatureEnabled('code', featureState)
-		// so we've done this instead
-		featureToggleService.grailsApplication.config.features.code.enabled = featureState
-		assert response.status == 200
-		assertEquals controller.modelAndView.model.result, "ran without feature"
-		println controller.modelAndView.model.result
-	}
+  @Test
+  void testCodeisDisabled() {
+    boolean featureState = featureToggleService.isFeatureEnabled('code')
+    featureToggleService.setFeatureEnabled('code', false)
+    withFilters(action: 'toggleInside') {
+      controller.toggleInside()
+    }
+    // return the value to what it used to be
+    featureToggleService.setFeatureEnabled('code', featureState)
+    assert response.status == 200
+    assertEquals controller.modelAndView.model.result, "ran without feature"
+    println controller.modelAndView.model.result
+  }
 
+  @Test
+  void testWithElseConstructEnabled() {
+    boolean featureState = featureToggleService.isFeatureEnabled('code')
+    featureToggleService.setFeatureEnabled('code', true)
+    withFilters(action: 'toggleInside') {
+      controller.useWithElseConstruct()
+    }
+    // return the value to what it used to be
+    featureToggleService.setFeatureEnabled('code', featureState)
+    assert response.status == 200
+    assertEquals controller.modelAndView.model.result, "ran with feature"
+    println controller.modelAndView.model.result
+  }
+  @Test
+  void testWithElseConstructDisabled() {
+    boolean featureState = featureToggleService.isFeatureEnabled('code')
+    featureToggleService.setFeatureEnabled('code', false)
+    withFilters(action: 'toggleInside') {
+      controller.useWithElseConstruct()
+    }
+    // return the value to what it used to be
+    featureToggleService.setFeatureEnabled('code', featureState)
+    assert response.status == 200
+    assertEquals controller.modelAndView.model.result, "ran without feature"
+    println controller.modelAndView.model.result
+  }
+  @Test
+  void testWithoutElseConstructEnabled() {
+    boolean featureState = featureToggleService.isFeatureEnabled('code')
+    featureToggleService.setFeatureEnabled('code', true)
+    withFilters(action: 'toggleInside') {
+      controller.useWithoutElseConstruct()
+    }
+    // return the value to what it used to be
+    featureToggleService.setFeatureEnabled('code', featureState)
+    assert response.status == 200
+    assertEquals controller.modelAndView.model.result, "ran with feature"
+    println controller.modelAndView.model.result
+  }
+  @Test
+  void testWithoutElseConstructDisabled() {
+    boolean featureState = featureToggleService.isFeatureEnabled('code')
+    featureToggleService.setFeatureEnabled('code', false)
+    withFilters(action: 'toggleInside') {
+      controller.useWithoutElseConstruct()
+    }
+    // return the value to what it used to be
+    featureToggleService.setFeatureEnabled('code', featureState)
+    assert response.status == 200
+    assertEquals controller.modelAndView.model.result, "ran without feature"
+    println controller.modelAndView.model.result
+  }
 }
