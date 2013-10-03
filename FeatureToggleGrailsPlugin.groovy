b1c1a80c937e19ff6612a97b776c0e46b791370b
David Kuhn <davidk@criticalmass.com>
2013-10-03 10:46:56 -0500

    Create release version

--- FeatureToggleGrailsPlugin.groovy
+++ FeatureToggleGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.plugin.featuretoggle.FeatureToggleService
 
 class FeatureToggleGrailsPlugin {
 	// the plugin version
-	def version = "0.18-SNAPSHOT"
+	def version = "0.18"
 	// the version or versions of Grails the plugin is designed for
 	def grailsVersion = "1.3.7 > *"
 	// the other plugins this plugin depends on
